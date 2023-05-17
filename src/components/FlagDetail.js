6e54e8fc26744e60f04126051f23a07fbe82a43a
Damian Ludef <damiinho@gmail.com>
2023-05-17 16:17:18 +0200

    data from api for flagdetail

--- src/components/FlagDetail.js
+++ src/components/FlagDetail.js
@@ -1,69 +1,186 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AppContext } from "../contexts/AppContext";
+import wikipediaIMG from "../img/Wikipedia.svg";
+import OSMapIMG from "../img/Openstreetmap.svg";
+import GMapsIMG from "../img/Google_Maps.svg";
+import TimezoneIMG from "../img/time-zone.png";
 
 const FlagDetail = () => {
   const { selectedSmallOne } = useContext(AppContext);
+  const [openStreetMapLink, setOpenStreetMapLink] = useState("");
+  const [officialName, setOfficialName] = useState("");
+  const [lat, setLat] = useState("");
+  const [lng, setLng] = useState("");
+  const [time, setTime] = useState("");
 
-  const API = "AIzaSyDXy8dIHe3_tMpWmOXAYGqbVgfXkd6_l-I";
-
+  ///////////////////////////
+  // restcountries section //
+  ///////////////////////////
   useEffect(() => {
-    if (selectedSmallOne) {
-      const fetchCoordinates = async () => {
-        try {
-          const response = await fetch(
-            `https://maps.googleapis.com/maps/api/geocode/json?address=${selectedSmallOne.name}&key=${API}`
-          );
-          const data = await response.json();
+    if (selectedSmallOne && selectedSmallOne.alpha3 !== "") {
+      const fetchData = async () => {
+        if (selectedSmallOne) {
+          try {
+            const response = await fetch(
+              `https://restcountries.com/v3/alpha/${encodeURIComponent(
+                selectedSmallOne.alpha3
+              )}`
+            );
+            const data = await response.json();
+            const openStreetMap = data[0]?.maps?.openStreetMaps;
+            setOpenStreetMapLink(openStreetMap || "");
 
-          // Pobranie współrzędnych geograficznych
-          const { lat, lng } = data.results[0].geometry.location;
-        } catch (error) {
-          console.error("Błąd podczas pobierania danych:", error);
+            const getOfficialName = (data) => {
+              const nativeNames = data[0]?.name.nativeName;
+              if (nativeNames) {
+                const officialName = Object.values(nativeNames).find(
+                  (name) => typeof name === "object" && "official" in name
+                );
+                if (officialName) {
+                  return officialName.official;
+                }
+              }
+              return "";
+            };
+            const officialName = getOfficialName(data);
+            setOfficialName(officialName);
+          } catch (error) {
+            console.error("Błąd pobierania danych po alpha3: ", error);
+          }
         }
       };
 
-      fetchCoordinates();
+      fetchData();
+    } else {
+      setOpenStreetMapLink("");
     }
-  }, [selectedSmallOne]);
+  }, [selectedSmallOne, openStreetMapLink]);
 
-  const fetchCountryInfo = async (countryName) => {
-    try {
-      const response = await fetch(
-        `https://restcountries.com/v3/name/${encodeURIComponent(countryName)}`
-      );
-      const data = await response.json();
-
-      // Sprawdź, czy istnieją dane dla podanej nazwy kraju
-      if (response.ok && data.length > 0) {
-        const country = data[0];
-        console.log("Informacje o kraju:", country);
-        // Tutaj możesz użyć pobranych danych do wyświetlenia informacji o kraju w aplikacji
+  //////////////////////
+  // TimeZone section //
+  //////////////////////
+  useEffect(() => {
+    const geonamesUsername = "damiinho";
+    if (selectedSmallOne) {
+      if (selectedSmallOne.englishCapital.length > 0) {
+        fetch(
+          `http://api.geonames.org/searchJSON?formatted=true&q=${selectedSmallOne.englishCapital[0]}&maxRows=1&username=${geonamesUsername}`
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            setLng(data.geonames[0].lng);
+            setLat(data.geonames[0].lat);
+          })
+          .catch((error) => {
+            setLat("");
+            setLng("");
+            console.log(
+              "Błąd podczas pobierania danych z angielskiej nazwy miasta:",
+              error
+            );
+          });
+      } else if (selectedSmallOne.englishName) {
+        fetch(
+          `http://api.geonames.org/searchJSON?formatted=true&q=${selectedSmallOne.englishName}&maxRows=1&username=${geonamesUsername}`
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            setLng(data.geonames[0].lng);
+            setLat(data.geonames[0].lat);
+          })
+          .catch((error) => {
+            setLat("");
+            setLng("");
+            console.log(
+              "Błąd podczas pobierania danych z angielskiej nazwy kraju:",
+              error
+            );
+          });
       } else {
-        console.error("Nie znaleziono informacji dla podanego kraju");
+        setLat("");
+        setLng("");
       }
-    } catch (error) {
-      console.error("Błąd podczas pobierania danych:", error);
-    }
-  };
 
-  fetchCountryInfo("Polska"); // Przykład użycia dla kraju "Polska"
+      if (lat !== "" && lng !== "") {
+        fetch(
+          `http://api.geonames.org/timezoneJSON?formatted=true&lat=${lat}&lng=${lng}&username=${geonamesUsername}`
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            setTime(data.time);
+          })
+          .catch((error) => {
+            console.log(
+              "Wystąpił błąd podczas pobierania danych czasu:",
+              error
+            );
+            setTime("");
+          });
+      } else setTime("");
+    }
+  }, [lat, lng, selectedSmallOne]);
+  const TimeZone = () => {
+    return (
+      <div className="App__flag-detail__timezones">
+        <div className="App__flag-detail__timezones-img">
+          <img src={TimezoneIMG} alt="timezones" />
+        </div>
+        {time}
+      </div>
+    );
+  };
+  const Link = (props) => (
+    <div className="App__flag-detail__link">
+      <div className="App__flag-detail__link-img">
+        <img src={props.img} alt={props.alt} />
+      </div>
+      <a href={props.link}>{props.title}</a>
+    </div>
+  );
 
   return (
     <>
       {selectedSmallOne ? (
         <div className="App__flag-detail">
-          {selectedSmallOne.name}
+          <h1>{selectedSmallOne.name}</h1>
+          {officialName && <h4>{officialName}</h4>}
           <img src={selectedSmallOne.img} alt="" />
-          <p>stolica: {selectedSmallOne.capital.join(", ")}</p>
+          {selectedSmallOne.capital.length !== 0 && (
+            <p>stolica: {selectedSmallOne.capital.join(", ")}</p>
+          )}
 
-          <a href={`https://pl.wikipedia.org/wiki/${selectedSmallOne.name}`}>
-            Przeczytaj więcej na Wikipedii
-          </a>
-          <a
-            href={`https://www.google.com/maps/place/${selectedSmallOne.name}`}
-          >
-            Znajdź na Google Maps
-          </a>
+          <Link
+            img={wikipediaIMG}
+            alt="wikipedia"
+            link={`https://pl.wikipedia.org/wiki/${selectedSmallOne.name}`}
+            title="Wikipedia"
+          />
+          <Link
+            img={GMapsIMG}
+            alt="google"
+            link={`https://www.google.com/maps/place/${selectedSmallOne.name}`}
+            title="Google Maps"
+          />
+
+          {openStreetMapLink && (
+            <Link
+              img={OSMapIMG}
+              alt="osmap"
+              link={openStreetMapLink}
+              title="OpenStreetMap"
+            />
+          )}
+          {time && <TimeZone />}
+          {/* dostęp do morza
+          powierzchnia
+          granice
+          populacja
+          strona drogi
+          waluta
+          onz
+          domena
+          z api geonames uzyskać kody lng i lat dla stolic, a jeśli nie ma, to dla krajów, a potem ustawić czas
+           */}
         </div>
       ) : null}
     </>
