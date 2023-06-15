16101b1bd5a9608c65aea2c9479a577b620580a7
Damian Ludef <damiinho@gmail.com>
2023-06-15 23:14:47 +0200

    delete titleBox for Game

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,13 +5,12 @@ import TitleBox from "./Header/TitleBox";
 import ResetButton from "./Header/ResetButton";
 
 const Header = () => {
-  const { windowHeight, windowWidth, isGame, isSelectors } =
-    useContext(AppContext);
+  const { windowWidth, isGame, isSelectors } = useContext(AppContext);
 
   return (
     <header className="App__header">
       {isGame ? (
-        windowHeight > 829 && <TitleBox />
+        ""
       ) : (
         <>
           <div className="header__title">
